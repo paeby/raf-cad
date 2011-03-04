b00dfcb680b646dc11279362fb3145f31ce20212
ikabiljo <ikabiljo@7b8d543c-1088-0afd-cfc5-4550c7f60786>
2011-03-04 15:44:35 +0000

    snapshot

--- ConcurrentTestFramework/src/common/ProcessInfo.java
+++ ConcurrentTestFramework/src/common/ProcessInfo.java
@@ -1,12 +1,18 @@
 package common;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProcessInfo {
 	final int currentId;
 	final int totalProcesses;
 	
+	final Map<Integer, Integer> threadLocals;
+	
 	public ProcessInfo(int currentId, int totalProcesses) {
 		this.currentId = currentId;
 		this.totalProcesses = totalProcesses;
+		this.threadLocals = new HashMap<Integer, Integer>();
 	}
 	
 	public int getCurrentId() {
@@ -17,5 +23,17 @@ public class ProcessInfo {
 		return totalProcesses;
 	}
 	
+	public Map<Integer, Integer> getThreadLocals() {
+		return threadLocals;
+	}
+	
+	public int getThreadLocal(int key) {
+		Integer value = threadLocals.get(key);
+		return value==null?0:value.intValue();
+	}
+
+	public void putThreadLocal(int key, int value) {
+		threadLocals.put(key, value);
+	}
 	
 }
--- ConcurrentTestFramework/src/solutions/snapshot/SnapshotSolutions.java
+++ ConcurrentTestFramework/src/solutions/snapshot/SnapshotSolutions.java
@@ -1,5 +1,7 @@
 package solutions.snapshot;
 
+import java.util.Arrays;
+
 import common.ConcurrentSystem;
 import common.ProcessInfo;
 import common.registers.Register;
@@ -9,30 +11,91 @@ import examples.snapshot.SnapshotTester;
 
 public class SnapshotSolutions {
 	private static final class SnapshotNaive implements Snapshot {
-		@Override
-		public int getArrayLength() {
-			return 10;
-		}
 		
 		@Override
-		public int[] getAllValues(ConcurrentSystem system, ProcessInfo callerInfo) {
-			int[] values = new int[getArrayLength()];
+		public int[] getAllValues(int arrayLength, ConcurrentSystem system, ProcessInfo callerInfo) {
+			int[] values = new int[arrayLength];
 			for (int i = 0; i < values.length; i++)
 				values[i] = system.getRegister(i).read();
 			return values;
 		}
 		
 		@Override
-		public void updateValue(int index, int value, ConcurrentSystem system, ProcessInfo callerInfo) {
+		public void updateValue(int index, byte value, int arrayLength, ConcurrentSystem system, ProcessInfo callerInfo) {
 			system.getRegister(index).write(value);
 		}
 	}
 	
-	private static final class SnapshotAtomic implements Snapshot {
-		@Override
-		public int getArrayLength() {
-			return 10;
+	private static final class SnapshotNoTimestamps implements Snapshot {
+		
+		private int[] collect(int length, ConcurrentSystem system) {
+			int[] res = new int[length];
+			for(int i = 0;i<length;i++) {
+				Register reg = system.getRegister(i);
+				res[i] = reg.read();
+			}
+			return res;
 		}
+		
+		
+		@Override
+		public int[] getAllValues(int length, ConcurrentSystem system, ProcessInfo callerInfo) {
+			int[] previous = collect(length, system);
+			
+			while (true) {
+				int[] cur = collect(length, system);
+				if (Arrays.equals(previous, cur))
+					return cur;
+				previous = cur;
+			}
+		}
+		
+		@Override
+		public void updateValue(int index, byte value, int length, ConcurrentSystem system, ProcessInfo callerInfo) {
+			Register reg = system.getRegister(index);
+			reg.write((value & 255));
+		}
+	}
+	
+	private static final class SnapshotCorrect implements Snapshot {
+	
+		private int[] collect(int length, ConcurrentSystem system) {
+			int[] res = new int[length];
+			for(int i = 0;i<length;i++) {
+				Register reg = system.getRegister(i);
+				res[i] = reg.read();
+			}
+			return res;
+		}
+		
+		
+		@Override
+		public int[] getAllValues(int length, ConcurrentSystem system, ProcessInfo callerInfo) {
+			int[] previous = collect(length, system);
+			
+			while (true) {
+				int[] cur = collect(length, system);
+				if (Arrays.equals(previous, cur)) {
+					int[] sol = new int[length];
+					for(int i = 0;i<length;i++)
+						sol[i] = cur[i] & 255;
+					return sol;
+				}
+				previous = cur;					
+			}
+		}
+		
+		@Override
+		public void updateValue(int index, byte value, int length, ConcurrentSystem system, ProcessInfo callerInfo) {
+			int ts = callerInfo.getThreadLocal(0);
+			callerInfo.putThreadLocal(0, ts+1);
+			
+			Register reg = system.getRegister(index);
+			reg.write((ts<<8)| (value & 255));
+		}
+	}
+	
+	private static final class SnapshotAtomic implements Snapshot {
 		
 		private void collect(int[][] values, ConcurrentSystem system, ProcessInfo callerInfo) {
 			for (int i = 0; i < values.length; i++) {
@@ -55,9 +118,9 @@ public class SnapshotSolutions {
 		}
 		
 		@Override
-		public int[] getAllValues(ConcurrentSystem system, ProcessInfo callerInfo) {
+		public int[] getAllValues(int arrayLength, ConcurrentSystem system, ProcessInfo callerInfo) {
 			int[][] temp1, temp2, helper;
-			temp1 = new int[getArrayLength()][];
+			temp1 = new int[arrayLength][];
 			temp2 = new int[temp1.length][];
 			for (int i = 0; i < temp1.length; i++) {
 				temp1[i] = new int[2];
@@ -81,29 +144,23 @@ public class SnapshotSolutions {
 		}
 		
 		@Override
-		public void updateValue(int index, int value, ConcurrentSystem system, ProcessInfo callerInfo) {
+		public void updateValue(int index, byte value, int arrayLength, ConcurrentSystem system, ProcessInfo callerInfo) {
 			Register tsRegister = system.getRegister(-system.getPID() - 1);
 			int ts = tsRegister.read();
 			tsRegister.write(++ts);
-			system.getRegister(index + getArrayLength()).write(ts);
+			system.getRegister(index + arrayLength).write(ts);
 			system.getRegister(index).write(value);
 		}
 	}
 	
 	private static final class SnapshotAtomicLockFree implements Snapshot {
-		private static final int ARRAY_LENGTH = 10;
 		
-		@Override
-		public int getArrayLength() {
-			return ARRAY_LENGTH;
-		}
-		
-		private void collect(int[][] values, ConcurrentSystem system, ProcessInfo callerInfo) {
-			for (int i = 0; i < ARRAY_LENGTH; i++) {
+		private void collect(int[][] values, int arrayLength, ConcurrentSystem system, ProcessInfo callerInfo) {
+			for (int i = 0; i < arrayLength; i++) {
 				values[i][0] = system.getRegister(i).read();
-				values[i][1] = system.getRegister(ARRAY_LENGTH + i).read();
-				for (int j = 0; j < ARRAY_LENGTH; j++)
-					values[i][j + 2] = system.getRegister(ARRAY_LENGTH * (2 + i) + j).read();
+				values[i][1] = system.getRegister(arrayLength + i).read();
+				for (int j = 0; j < arrayLength; j++)
+					values[i][j + 2] = system.getRegister(arrayLength * (2 + i) + j).read();
 			}
 		}
 		
@@ -127,31 +184,31 @@ public class SnapshotSolutions {
 		}
 		
 		@Override
-		public int[] getAllValues(ConcurrentSystem system, ProcessInfo callerInfo) {
+		public int[] getAllValues(int arrayLength, ConcurrentSystem system, ProcessInfo callerInfo) {
 			int[][] temp1, temp2, temp3, helper;
-			temp1 = new int[ARRAY_LENGTH][];
-			temp2 = new int[ARRAY_LENGTH][];
-			temp3 = new int[ARRAY_LENGTH][];
-			for (int i = 0; i < ARRAY_LENGTH; i++) {
-				temp1[i] = new int[(2 + ARRAY_LENGTH) * ARRAY_LENGTH];
-				temp2[i] = new int[(2 + ARRAY_LENGTH) * ARRAY_LENGTH];
-				temp3[i] = new int[(2 + ARRAY_LENGTH) * ARRAY_LENGTH];
+			temp1 = new int[arrayLength][];
+			temp2 = new int[arrayLength][];
+			temp3 = new int[arrayLength][];
+			for (int i = 0; i < arrayLength; i++) {
+				temp1[i] = new int[(2 + arrayLength) * arrayLength];
+				temp2[i] = new int[(2 + arrayLength) * arrayLength];
+				temp3[i] = new int[(2 + arrayLength) * arrayLength];
 			}
 			
-			collect(temp1, system, callerInfo);
+			collect(temp1, arrayLength, system, callerInfo);
 			copyMatrix(temp2, temp1);
 			while (true) {
-				collect(temp3, system, callerInfo);
+				collect(temp3, arrayLength, system, callerInfo);
 				if (areMatricesEqual(temp3, temp2)) {
-					int[] result = new int[ARRAY_LENGTH];
-					for (int i = 0; i < ARRAY_LENGTH; i++)
+					int[] result = new int[arrayLength];
+					for (int i = 0; i < arrayLength; i++)
 						result[i] = temp3[i][0];
 					return result;
 				}
-				for (int i = 0; i < ARRAY_LENGTH; i++)
+				for (int i = 0; i < arrayLength; i++)
 					if (temp3[i][1] >= temp1[i][1] + 2) {
-						int[] result = new int[ARRAY_LENGTH];
-						for (int j = 0; j < ARRAY_LENGTH; j++)
+						int[] result = new int[arrayLength];
+						for (int j = 0; j < arrayLength; j++)
 							result[j] = temp3[i][j + 2];
 						return result;
 					}
@@ -162,15 +219,15 @@ public class SnapshotSolutions {
 		}
 		
 		@Override
-		public void updateValue(int index, int value, ConcurrentSystem system, ProcessInfo callerInfo) {
+		public void updateValue(int index, byte value, int arrayLength, ConcurrentSystem system, ProcessInfo callerInfo) {
 			Register tsRegister = system.getRegister(-system.getPID() - 1);
 			int ts = tsRegister.read();
 			tsRegister.write(++ts);
 			
-			int[] snapshot = getAllValues(system, callerInfo);
-			for (int i = ARRAY_LENGTH - 1; i >= 0; i--)
-				system.getRegister(ARRAY_LENGTH * (index + 2) + i).write(snapshot[i]);
-			system.getRegister(index + ARRAY_LENGTH).write(ts);
+			int[] snapshot = getAllValues(arrayLength, system, callerInfo);
+			for (int i = arrayLength - 1; i >= 0; i--)
+				system.getRegister(arrayLength * (index + 2) + i).write(snapshot[i]);
+			system.getRegister(index + arrayLength).write(ts);
 			system.getRegister(index).write(value);
 		}
 	}
@@ -179,6 +236,10 @@ public class SnapshotSolutions {
 	public static void main(String[] args) {
 		System.out.println("Naive:");
 		SnapshotTester.testSnapshot(new SnapshotNaive());
+		System.out.println("Without timestamps:");
+		SnapshotTester.testSnapshot(new SnapshotNoTimestamps());
+		System.out.println("Simple correct:");
+		SnapshotTester.testSnapshot(new SnapshotCorrect());
 		System.out.println("Atomic but with starvation:");
 		SnapshotTester.testSnapshot(new SnapshotAtomic());
 		System.out.println("Atomic & lock free:");
