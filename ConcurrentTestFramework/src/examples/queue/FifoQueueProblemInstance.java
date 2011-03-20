d7089cfda4443018a649fa894658bfd894e852ca
ikabiljo <ikabiljo@7b8d543c-1088-0afd-cfc5-4550c7f60786>
2011-03-20 00:34:28 +0000

    fifo fixes

--- ConcurrentTestFramework/src/examples/queue/FifoQueueProblemInstance.java
+++ ConcurrentTestFramework/src/examples/queue/FifoQueueProblemInstance.java
@@ -1,5 +1,6 @@
 package examples.queue;
 
+import java.util.HashSet;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -14,19 +15,26 @@ public class FifoQueueProblemInstance implements ProblemInstance<FifoQueue> {
 	private final int howManyOperations, howManyProcesses;
 	private final boolean addThenRemove;
 	
-	public FifoQueueProblemInstance(int processes, int operations, boolean addTheRemove) {
+	public FifoQueueProblemInstance(int processes, int operations, boolean addThenRemove) {
 		this.howManyProcesses = processes;
 		this.howManyOperations = operations;
-		this.addThenRemove = addTheRemove;
+		this.addThenRemove = addThenRemove;
 	}
 	
 	@Override
 	public boolean execute(final ConcurrentManagedSystem managedSystem, final FifoQueue solution) {
 		final AtomicBoolean correct = new AtomicBoolean(true);
-		final AtomicInteger[] readersAreHere = new AtomicInteger[howManyProcesses];
-		for (int i = 0; i < howManyProcesses; i++)
-			readersAreHere[i] = new AtomicInteger(-1);
-		final AtomicInteger totalCountOfReads = new AtomicInteger(0);
+		final HashSet<Integer> insertedValues = new HashSet<Integer>(); 
+		
+		final AtomicInteger addsStarted = new AtomicInteger(1);
+		final AtomicInteger addsDone = new AtomicInteger(1);
+		final int[] addLower = new int[(howManyOperations + 1) * (howManyProcesses + 1)];
+		final int[] addHigher = new int[(howManyOperations + 1) * (howManyProcesses + 1)];
+		
+		final AtomicInteger removesStarted = new AtomicInteger(1);
+		final AtomicInteger removesDone = new AtomicInteger(1);
+		
+		
 		final AtomicInteger addingDone = new AtomicInteger(howManyProcesses);
 		
 		for (int threadIndex = 0; threadIndex < howManyProcesses; threadIndex++) {
@@ -38,9 +46,15 @@ public class FifoQueueProblemInstance implements ProblemInstance<FifoQueue> {
 				public void execute(ConcurrentSystem system) {
 					int current = threadId;
 					for (int i = 0; correct.get() && i < howManyOperations; i++) {
+						addLower[current] = addsDone.get();
+						addHigher[current] = Integer.MAX_VALUE;
+						addsStarted.incrementAndGet();
+						insertedValues.add(current);
 						managedSystem.addLogLine("\t\t\tcid=" + callerInfo.getCurrentId() + " adding value " + current);
 						solution.add(current, managedSystem, callerInfo);
 						managedSystem.addLogLine("\t\t\tcid=" + callerInfo.getCurrentId() + " value " + current + " added.");
+						addHigher[current] = addsStarted.get();
+						addsDone.incrementAndGet();
 						current += howManyProcesses;
 					}
 					addingDone.decrementAndGet();
@@ -57,22 +71,30 @@ public class FifoQueueProblemInstance implements ProblemInstance<FifoQueue> {
 					if (addThenRemove)
 						while (addingDone.get() != 0)
 							system.yield();
-					while (correct.get() && totalCountOfReads.get() < howManyProcesses * howManyOperations) {
+					while (correct.get() && removesDone.get() <= howManyProcesses * howManyOperations) {
+
+						int lower = removesDone.get();
+						removesStarted.incrementAndGet();
+						
 						managedSystem.addLogLine("\t\t\tcid=" + callerInfo.getCurrentId() + " removing value");
 						int value = solution.remove(managedSystem, callerInfo);
 						if (value == -1) {
-							managedSystem.addLogLine("\t\t\tcid=" + callerInfo.getCurrentId() + " read from empty queue.");
+							managedSystem.addLogLine("\t\t\tcid=" + callerInfo.getCurrentId() + " tried to remove from empty queue.");
+							removesStarted.decrementAndGet();
 							continue;
 						}
-						totalCountOfReads.incrementAndGet();
-						managedSystem.addLogLine("\t\t\tcid=" + callerInfo.getCurrentId() + " read value " + value);
+						managedSystem.addLogLine("\t\t\tcid=" + callerInfo.getCurrentId() + " removed value " + value);
 						
-						AtomicInteger oldValueFromThatModus = readersAreHere[value % howManyProcesses];
-						if (oldValueFromThatModus.get() >= value) {
-							managedSystem.addLogLine("\t\t\tcid=" + callerInfo.getCurrentId() + " **** read value " + value + " which has been inserted after " + oldValueFromThatModus.get());
+						removesDone.incrementAndGet();
+						int higher = removesStarted.get();
+						
+						if (!insertedValues.remove(value)) {
+							managedSystem.addLogLine("\t\t\tcid=" + callerInfo.getCurrentId() + " **** removed value " + value + " which hasn't been inserted");
 							correct.set(false);
-						} else {
-							oldValueFromThatModus.set(value);
+						} else if (higher < addLower[value] || addHigher[value] < lower) {
+							managedSystem.addLogLine("\t\t\tcid=" + callerInfo.getCurrentId() + " **** removed value " + value + " at wrong time, possible intervals don't intersect " 
+									+ "[" + lower + "," + higher +"] and [" + addLower[value] + "," +  addHigher[value] +"]");
+							correct.set(false);
 						}
 					}
 					int value = solution.remove(managedSystem, callerInfo);
--- ConcurrentTestFramework/src/examples/queue/FifoQueueTester.java
+++ ConcurrentTestFramework/src/examples/queue/FifoQueueTester.java
@@ -9,14 +9,14 @@ public class FifoQueueTester {
 		System.out.println("Dummy test");
 		if (!ProblemTester.testProblem(new FifoQueueProblemInstance(1, 10, true), fifoQueue, 100))
 			return;
+		System.out.println("Shallow correctness test");
+		if (!ProblemTester.testProblem(new FifoQueueProblemInstance(2, 3, false), fifoQueue, 200))
+			return;
 		System.out.println("Add all then remove all test");
 		if (!ProblemTester.testProblem(new FifoQueueProblemInstance(2, 20, true), fifoQueue, 100))
 			return;
 		System.out.println("Simultaneous reading and writing");
 		if (!ProblemTester.testProblem(new FifoQueueProblemInstance(1, 5, false), fifoQueue, 200))
-			return;
-		System.out.println("Shallow correctness test");
-		if (!ProblemTester.testProblem(new FifoQueueProblemInstance(2, 3, false), fifoQueue, 200))
 			return;
 		System.out.println("Thorough test");
 		if (!ProblemTester.testProblem(new FifoQueueProblemInstance(5, 100, false), fifoQueue, 30))
